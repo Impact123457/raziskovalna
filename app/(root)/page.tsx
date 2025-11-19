8db58795492237e1070bbc9d60502ebd8ad4512c
Impact123457 <ema.skruba@scv.si>
2025-11-19 10:11:40 +0100

    test

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,58 +2,13 @@ import Image from "next/image";
 export default function Home() {
   return (
   <>
-    <div className="flex">
-      
-      <div className="divH1">
+    <div className="items-center bg-blue-800 h-[400px] flex flex-col justify-center text-center">
         <h1 className="heading">Welcome to WebInspector</h1>
-      </div>
-
-      <div className="divImg">
-        <div className="w-[35%]">
-          <Image src="/robot.png" alt="robot" width={700} height={700}/>
-        </div>
-          
-        <div className="w-[65%]">
           <p className="opis">
-            <b>WebInspector</b> 
-            This tool analyzes any website you submit and provides a clear report on its accessibility, usability, and overall technical health. 
-            It quickly identifies issues that may affect performance or user experience, highlights areas that do not meet modern accessibility standards, and offers 
-            actionable insights to ensure the site works properly and delivers a smooth, inclusive experience for all users.
+            WebInspector
+            This tool analyzes any website you submit and provides a clear report on its accessibility, usability. It quickly identifies issues that may affect user experience, highlights areas that do not meet modern accessibility standards, and offers 
+            actionable insights to ensure the site works properly.
           </p>
-        </div>
-
-      </div>  
-    </div>
-
-    <div className="div2">
-
-      <div className="flex justify-center w-[30%]">
-        <Image src="/robot-stand.png" alt="robot" width={500} height={500}/>
-      </div>
-
-      <div className="w-[70%]">
-
-          <div className="card">
-            <h1 className="cardH1">Website Analysis</h1>
-            
-            <p className="w-[65%]">Users can submit any website link to get a detailed evaluation.</p>
-          </div>
-          
-          <div className="card">
-            <h1 className="cardH1">Accessibility Checks</h1>
-            
-            <p className="w-[65%]">It identifies accessibility issues and 
-              shows whether the site meets modern standards for users with disabilities.</p>
-          </div>
-
-          <div className="card">
-            <h1 className="cardH1">Actionable Insights</h1>
-            
-            <p className="w-[65%]">It provides clear recommendations to fix issues and improve the 
-              site is overall performance, accessibility, and usability.</p>
-          </div>
-
-      </div>    
     </div>
   </>
   )
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,10 +2,10 @@ import Link from "next/link"
 import Image from "next/image"
 const Navbar = async() =>{   
     return(
-        <header className="px-5 py-5 bg-white shadow-sm font-work-sans">
+        <header className="bg-white shadow-sm font-work-sans">
             <nav className="flex justify-between items-center">
                 <Link href="/">
-                    <Image src="/logo.png" alt="logo" width={100} height={100}></Image>
+                    <Image src="/logo.png" alt="logo" width={80} height={80}></Image>
                 </Link>
                 
             </nav>
